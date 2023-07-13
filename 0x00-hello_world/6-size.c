8d77ba0d78b3a6efc24aa4785c48a58422df672d
John <132560971+Krokuta001@users.noreply.github.com>
2023-07-13 13:27:13 +0300

    final

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
@@ -10,4 +15,5 @@ int main(void)
 
 	return (0);
 }
+
 
