173183abc75dcce322d23baa674b1cef4dcc664f
John <132560971+Krokuta001@users.noreply.github.com>
2023-07-14 15:39:07 +0300

    final

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,39 +1,36 @@
-/*
- * File: 101-print_comb4.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - Prints all possible combinations of three different digits,
- *        in ascending order, separated by a comma followed by a space.
+ * main - A program that prints combination of all three digit numbers.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int digit1, digit2, digit3;
+	int hundreds;
+	int tens;
+	int ones;
+	int num;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
+	for (num = 0; num < 1000; num++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+		hundreds = num / 100;
+		tens = (num / 10) % 10;
+		ones = num % 10;
 
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
+		if (hundreds < tens && tens < ones)
+		{
+			putchar(hundreds + '0');
+			putchar(tens + '0');
+			putchar(ones + '0');
+
+			if (num < 700)
+			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-
 	putchar('\n');
 
 	return (0);
