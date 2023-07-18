377564aebc3a6b07eae66a8d3d0679cc28d7eb27
John <kingkrokuta@gmail.com>
2023-07-18 05:52:02 -0700

    final

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/*a function that adds two integers and returns the result.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	n = add(80, 9);
+        printf("%d\n", n);
+        return (0);
+}
