f37dad9bab70cf10444848936f2197d591865934
John <132560971+Krokuta001@users.noreply.github.com>
2023-07-31 17:56:33 +0300

    final

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,24 +1,18 @@
-/*
- * 0x07. C - Even more pointers, arrays and strings
- * task 7
- */
-#include "main.h"
 #include <stdio.h>
-#include <string.h>
+
 /**
- * print_chessboard - returns chessboard
- * @a: array refrance
+ * print_chessboard - prints the chessboard
+ *
+ * @a: 2D array representing the chessboard
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-
-	for (i = 0; i < 8; i++)
-	{
-		for (j = 0; j < 8; j++)
-		{
-			printf("%c", a[i][j]);
-		}
-		printf("\n");
-	}
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            putchar(a[i][j]);
+        }
+        putchar('\n');
+    }
 }
