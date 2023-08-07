a2f646300746437dad0786136cc0e57b5e6a7479
John <132560971+Krokuta001@users.noreply.github.com>
2023-08-07 21:38:16 +0300

    final

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,13 +1,12 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 /**
- * strtow - concatenates all the arguments of your program
- * @str: string
- * @av: arguments
- * Return: a pointer to a new string
+ * strtow - Splits a string into words
+ * @str: The string to be split
+ *
+ * Return: A pointer to a new array of strings (words)
  */
 char **strtow(char *str)
 {
@@ -19,50 +18,56 @@ char **strtow(char *str)
     j = 0;
     i = 0;
     count = 0;
+    
     if (str == NULL || *str == '\0')
         return (NULL);
+
     for (i = 0; str[i] != '\0'; i++)
     {
         if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
             w++;
     }
+
     p = (char **)malloc((w + 1) * sizeof(char *));
     if (p == NULL)
         return (NULL);
+
     for (wordf = 0; str[wordf] && j <= w; wordf++)
     {
         count = 0;
+
         if (str[wordf] != ' ')
         {
-            for (i = wordf ; str[i] != '\0'; i++)
+            for (i = wordf; str[i] != '\0'; i++)
             {
                 if (str[i] == ' ')
                     break;
                 count++;
             }
-            if (count > 0)
+
+            *(p + j) = (char *)malloc((count + 1) * sizeof(char));
+            if (*(p + j) == NULL)
             {
-                *(p + j) = (char *)malloc((count + 1) * sizeof(char));
-                if (*(p + j) == NULL)
+                for (k = 0; k <= j; k++)
                 {
-                    for (k = 0; k <= j; k++)
-                    {
-                        x = p[k];
-                        free(x);
-                    }
-                    free(p);
-                    return (NULL);
+                    x = p[k];
+                    free(x);
                 }
-                for (m = 0; wordf < i; wordf++)
-                {
-                    p[j][m] = str[wordf];
-                    m++;
-                }
-                p[j][m] = '\0';
-                j++;
+                free(p);
+                return (NULL);
             }
+
+            for (m = 0; wordf < i; wordf++)
+            {
+                p[j][m] = str[wordf];
+                m++;
+            }
+
+            p[j][m] = '\0';
+            j++;
         }
     }
+
     p[j] = NULL;
     return (p);
 }
