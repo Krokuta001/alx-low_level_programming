7096357e585f86b76e3beb7b732f043af6d1753d
John <132560971+Krokuta001@users.noreply.github.com>
2023-08-07 21:14:52 +0300

    final

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,65 +1,68 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdbool.h> // Include the header for using bool type
 
 /**
  * strtow - concatenates all the arguments of your program
- *@str: string
- *@av: arguments
+ * @str: string
+ * @av: arguments
  * Return: a pointer to a new string
  */
 char **strtow(char *str)
 {
-	int i, w, j, k, count, m, wordf;
-	char **p;
-	char *x;
+    int i, w, j, k, count, m, wordf;
+    char **p;
+    char *x;
 
-	w = 0;
-	j = 0;
-	i = 0;
-	count = 0;
-	if (*str == '\0' || str == NULL)
-		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
-			w++;
-	}
-	p = (char **)malloc((w + 1) * sizeof(char *));
-	if (p == NULL)
-		return (NULL);
-	for (wordf = 0; str[wordf] && j <= w; wordf++)
-	{
-		count = 0;
-		if (str[wordf] != ' ')
-		{
-			for (i = wordf ; str[i] != '\0'; i++)
-			{
-				if (str[i] == ' ')
-					break;
-				count++;
-			}
-			*(p + j) = (char *)malloc((count + 1) * sizeof(char));
-			if (*(p + j) == NULL)
-			{
-				for (k = 0; k <= j; k++)
-				{
-					x = p[k];
-					free(x);
-				}
-				free(p);
-				return (NULL);
-			}
-			for (m = 0; wordf < i; wordf++)
-			{
-				p[j][m] = str[wordf];
-				m++;
-			}
-			p[j][m] = '\0';
-			j++;
-		}
-	}
-	p[j] = NULL;
-	return (p);
+    w = 0;
+    j = 0;
+    i = 0;
+    count = 0;
+    if (str == NULL || *str == '\0') // Check for NULL or empty string
+        return (NULL);
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
+            w++;
+    }
+    p = (char **)malloc((w + 1) * sizeof(char *));
+    if (p == NULL)
+        return (NULL);
+    for (wordf = 0; str[wordf] && j <= w; wordf++)
+    {
+        count = 0;
+        if (str[wordf] != ' ')
+        {
+            for (i = wordf ; str[i] != '\0'; i++)
+            {
+                if (str[i] == ' ')
+                    break;
+                count++;
+            }
+            if (count > 0) // Only allocate memory if word length is greater than 0
+            {
+                *(p + j) = (char *)malloc((count + 1) * sizeof(char));
+                if (*(p + j) == NULL)
+                {
+                    for (k = 0; k <= j; k++)
+                    {
+                        x = p[k];
+                        free(x);
+                    }
+                    free(p);
+                    return (NULL);
+                }
+                for (m = 0; wordf < i; wordf++)
+                {
+                    p[j][m] = str[wordf];
+                    m++;
+                }
+                p[j][m] = '\0';
+                j++;
+            }
+        }
+    }
+    p[j] = NULL;
+    return (p);
 }
