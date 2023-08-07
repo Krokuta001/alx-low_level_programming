1ac1801b411ca41e355d0fa5dddf05663e1c3d2c
John <kingkrokuta@gmail.com>
2023-08-07 10:42:43 -0700

    final

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,35 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
- * create_array - create an arry of size with char c
- * @size: unsigned int type
- * @c: char type
- * Return: Return pointer to array created
+ * create_array - Entry point
+ *@size: size of the array
+ *@c: character
+ * Return: a pointer to the array, or NULL if it fails
  */
-
 char *create_array(unsigned int size, char c)
 {
-	char *s;
+	char *array = NULL;
 	unsigned int i;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
-
-	s = malloc((size) * sizeof(char));
-	if (s == NULL)
+	if (size != 0)
 	{
-		return (NULL);
+		array = (char *)malloc(size * sizeof(char));
+		if (array != NULL)
+		{
+			for (i = 0; i < size; i++)
+				array[i] = c;
+		}
 	}
-	i = 0;
-	while (i < size)
-	{
-		s[i] = c;
-		i++;
-	}
-	s[i] = '\0';
-	return (s);
+	return (array);
 }
