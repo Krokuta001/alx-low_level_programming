6dfe7ae11e8367d60cf977e0b982a290f9ff32b3
John <132560971+Krokuta001@users.noreply.github.com>
2023-08-16 22:53:32 +0300

    final

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,74 +1,53 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * op_add - this function adds two numbers
- * @a:the first number
- * @b:the second number.
-(* a blank line
-* Description: this function adds two numbers)?
-(* section header: 3-calc.h)*
-* Return: return the result of the add.
-*/
+ * op_add - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 /**
- * op_sub - this function sub two numbers
- * @a:the first number
- * @b:the second number.
-(* a blank line
-* Description: this function sub two numbers)?
-(* section header: 3-calc.h)*
-* Return: return the result of the sub.
+ * op_sub - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
- * op_mul - this function multiply two numbers
- * @a:the first number
- * @b:the second number.
-(* a blank line
-* Description: this function multiply two numbers)?
-(* section header: 3-calc.h)*
-* Return: return the result of the multiply.
-*/
+ * op_mul - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 /**
- * op_div - this function divide two numbers
- * @a:the first number
- * @b:the second number.
-(* a blank line
-* Description: this function divide two numbers)?
-(* section header: 3-calc.h)*
-* Return: return the result of the divide.
+ * op_div - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
  */
 int op_div(int a, int b)
 {
-	if (b != 0)
-		return (a / b);
-	printf("Error\n");
-	exit(100);
+	return (a / b);
 }
 /**
- * op_mod - this function calcualte the module
- * @a:the first number
- * @b:the second number.
-(* a blank line
-* Description: this function calculate the module)?
-(* section header: 3-calc.h)*
-* Return: return the result of the module.
-*/
+ * op_mod - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
