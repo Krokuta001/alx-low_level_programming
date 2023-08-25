e62432baab918f3400c8aeceadb0a4a053e736fc
Krokuta001 <kingkrokuta@gmail.com>
2023-08-25 05:00:35 -0700

    final

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+/*
+ * File: rewritten-print-list.c
+ * Auth: [Your Name]
+ */
+
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Outputs the content of each element in a list_t list.
+ * @list: The list_t list.
+ *
+ * Return: The number of elements in the list.
+ */
+
+size_t print_list(const list_t *list)
+{
+	size_t element_count = 0;
+
+	while (list)
+	{
+		if (list->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", list->len, list->str);
+
+		element_count++;
+		list = list->next;
+	}
+
+	return (element_count);
+}
+
