e130e2b0d6f9fb66bebaa3fb3559f648c4242508
John <132560971+Krokuta001@users.noreply.github.com>
2023-08-25 08:27:14 -0400

    final

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,10 @@
 
 void __attribute__((constructor)) hare(void);
 
+/**
+ * Prints a string before the
+ * main function is executed.
+ */
 void hare(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
