93af71ebdf13d4a49d7102bcadd0ba61815c9061
John <132560971+Krokuta001@users.noreply.github.com>
2023-08-31 14:35:52 -0400

    final

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,47 @@
-/*
- * Auth:Krokuta
- * File: 0-binary_to_uint.c
- */
-
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
- */
+  * binary_to_uint - Converts a binary number to an unsigned int
+  * @b: The binary string to converts
+  *
+  * Return: The positive number converted from a binary
+  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int len = 0, count = 0, sum = 0;
 
-	if (b == '\0')
+	if (b == NULL)
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	len = _strlen(b);
+	while (len--)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[len] != 48 && b[len] != 49)
 			return (0);
 
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		if (b[len] == 49)
+			sum += 1 << count;
+
+		count++;
 	}
 
-	return (num);
+	return (sum);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(const char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
 }
